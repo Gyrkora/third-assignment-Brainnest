95c78a8c217c843adaa86b8d7c85776eedd894c6
hadaroadams <hadaroadams1234@gmail.com>
2023-03-12 15:24:09 +0000

    fixed cancel for user game request input

--- main.js
+++ main.js
@@ -1,55 +1,80 @@
+const rps = ['rock', 'paper', 'scissors'];
+const rpsDatabase = {
+		'rock': { 'rock': 0.5, 'paper': 0, 'scissors': 1 },
+		'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+		'scissors': { 'rock': 0, 'paper': 1, 'scissors': 0.5 },
+	};
 //The initial Score before being incremented
 let botWin = 0;
 let playerWin = 0;
 
+
+function handlesUserGameRequestInputs(){
+    let playerSelection = confirm("Hi this is a rock, paper and Scissors game, press okay if your want to play?");
+    if(playerSelection){
+		uservali()
+	}
+	else{
+		throw console.log( "If You decide to play the Game kindly refresh the page")	
+		}
+	}
+handlesUserGameRequestInputs()
+
 //user name validation
 function uservali() {
 	user = prompt('Hello! what is your name?');
-	if (user === '') {
+	if (user === "") {
 		alert('Please Enter Your Name');
 		uservali();
-	} else {
-		return user;
+	}else if(!user){
+	     let conf= confirm("Sorry but you pressed Cancel!:( Press Okay to Enter name or Cancel to exit")
+		 if(conf){
+			uservali()
+		 }else{
+			throw console.log( "If You decide to play the Game kindly refresh the page")
+		 }
+	}
+	else{
+		return user
 	}
 }
-uservali();
 
-alert(`Welcome to Rock, Paper, Scissors game ${user} click OK! to begin `);
+alert(`Welcome to Rock, Paper, Scissors game ${user} click OK! to begin `)
+
 
 //The function to allow Bot to choose randomly
 function computerPlay() {
-	let rps, botChoice, random;
-	rps = ['rock', 'paper', 'scissors'];
-	random = Math.floor(Math.random() * 3);
-	botChoice = rps[random];
-	return botChoice;
+	let randomSelection = rps[Math.floor(Math.random() * 3)];
+	return randomSelection;
 }
 
 //The playRound function that return and object with botScore and playerScore
 function playRound(playerSelection, computerSelection) {
-	let rpsDatabase, message, botScore;
-	rpsDatabase = {
-		rock: { rock: 0.5, paper: 0, scissors: 1 },
-		paper: { rock: 1, paper: 0.5, scissors: 0 },
-		scissors: { rock: 0, paper: 1, scissors: 0.5 },
-	};
+	let playerScore, botScore;
 
-	(botScore = rpsDatabase[computerSelection][playerSelection]),
-		(playerScore = rpsDatabase[playerSelection][computerSelection]);
+	;
 
-	console.log(`Bot chose ${computerSelection}`);
-	console.log(`You chose! ${player}`);
+	botScore = rpsDatabase[computerSelection][playerSelection],
+	playerScore = rpsDatabase[playerSelection][computerSelection];
+
+	
 
 	if (botScore === 1) {
 		message = `Bot won! ${computerSelection} beat ${playerSelection}`;
+		console.log(`Bot chose ${computerSelection}`);
+		console.log(`You chose! ${player}`);
 		console.log(message);
 		return message, { botScore: 1, playerScore: 0 };
 	} else if (botScore === 0.5) {
 		message = `You tied!`;
+		console.log(`Bot chose ${computerSelection}`);
+	console.log(`You chose! ${player}`);
 		console.log(message);
 		return { botScore: 0, playerScore: 0 };
 	} else {
 		message = `You won! ${playerSelection} beat ${computerSelection}`;
+		console.log(`Bot chose ${computerSelection}`);
+		console.log(`You chose! ${player}`);
 		console.log(message);
 		return { botScore: 0, playerScore: 1 };
 	}
@@ -57,11 +82,10 @@ function playRound(playerSelection, computerSelection) {
 
 function rpsGame() {
 	let botChoice, score;
-	rps = ['rock', 'paper', 'scissors'];
 
 	//Bot and users choice being assigned to variables
 	botChoice = computerPlay();
-	player = prompt('Choose Rock, Paper or Scissors').toLowerCase();
+	player = prompt('Choose Rock, Paper or Scissors').toLowerCase().trim();
 
 	//validation of rps selection
 	if (player === rps[0] || player === rps[1] || player === rps[2]) {
