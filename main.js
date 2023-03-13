a6002fc001def0ec3a313f637d718a51ec00a3b7
Gyrkora <giselle.gallardo0210@gmail.com>
2023-03-12 21:40:11 -0300

    nothing

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
+//global variables
 const rps = ['rock', 'paper', 'scissors'];
 const rpsDatabase = {
-		'rock': { 'rock': 0.5, 'paper': 0, 'scissors': 1 },
-		'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
-		'scissors': { 'rock': 0, 'paper': 1, 'scissors': 0.5 },
-	};
+	rock: { rock: 0.5, paper: 0, scissors: 1 },
+	paper: { rock: 1, paper: 0.5, scissors: 0 },
+	scissors: { rock: 0, paper: 1, scissors: 0.5 },
+};
 //The initial Score before being incremented
 let botWin = 0;
 let playerWin = 0;
 
 //This funtion handles the users confirmation to play the game
-function handlesUserGameRequestInputs(){
-    let playerConfirm = confirm("Hi this is a rock, paper and scissors game, press OK if your want to play?");
-    if(playerConfirm){
-		uservali()
+function handlesUserGameRequestInputs() {
+	let playerConfirm = confirm(
+		'Hi this is a rock, paper and scissors game, press OK if your want to play?'
+	);
+
+	if (playerConfirm) {
+		uservali();
+	} else {
+		throw console.log('If You decide to play the Game kindly refresh the page');
 	}
-	else{
-		throw console.log( "If You decide to play the Game kindly refresh the page")	
-		}
-	}
-handlesUserGameRequestInputs()
+}
+handlesUserGameRequestInputs();
 
 //user name validation
 function uservali() {
 	user = prompt('Hello! what is your name?');
-	if (user === "") {
+	if (user === '') {
 		alert('Please Enter Your Name');
 		uservali();
-	}else if(!user){
-	     let conf= confirm('Sorry but you pressed "Cancel!":( Press "Ok"to Enter name or Cancel to exit')
-		 if(conf){
-			uservali()
-		 }else{
-			throw console.log( "If You decide to play the Game kindly refresh the page to begin")
-		 }
-	}
-	else{
-		return user
+	} else if (!user) {
+		let conf = confirm(
+			'Sorry but you pressed "Cancel!":( Press "Ok"to Enter name or Cancel to exit'
+		);
+		if (conf) {
+			uservali();
+		} else {
+			throw console.log(
+				'If You decide to play the Game kindly refresh the page to begin'
+			);
+		}
+	} else {
+		return user;
 	}
 }
 
-alert(`Welcome to Rock, Paper, Scissors game ${user} click "OK!" to begin `)
-
+alert(`Welcome to Rock, Paper, Scissors game ${user} click "OK!" to begin `);
 
 //The function to allow Bot to choose randomly
 function computerPlay() {
@@ -48,30 +53,32 @@ function computerPlay() {
 	return randomSelection;
 }
 // This funtion takes return  the usersChoice with what happens when he decide to click okay or cancel
-function userChoice(){
-  let playerChoice = prompt('Choose Rock, Paper or Scissors')
-  
-  if(playerChoice === null){
-    let roundconf= confirm('You pressed "Cancel", press "Ok" to go back or "Cancel" to close the Game')
-	if(roundconf){
-		userChoice()
-	}
-	else{
-		console.clear()
-		throw console.log( "If You decide to play the Game kindly refresh the page to begin")
-	}
-  }
-  
-  playerChoice = playerChoice.toLowerCase().trim();
-  
-  if(!rps.includes(playerChoice)){
-    alert("Incorrect entry ");
-    return userChoice()
-  }
-  
-  return playerChoice
-}
+function userChoice() {
+	let playerChoice = prompt('Choose Rock, Paper or Scissors');
 
+	if (playerChoice === null) {
+		let roundconf = confirm(
+			'You pressed "Cancel", press "Ok" to go back or "Cancel" to close the Game'
+		);
+		if (roundconf) {
+			userChoice();
+		} else {
+			console.clear();
+			throw console.log(
+				'If You decide to play the Game kindly refresh the page to begin'
+			);
+		}
+	}
+
+	playerChoice = playerChoice.toLowerCase().trim();
+
+	if (!rps.includes(playerChoice)) {
+		alert('Incorrect entry ');
+		return userChoice();
+	}
+
+	return playerChoice;
+}
 
 //The playRound function take both the bots and user's choice and return an object with botScore and playerScore
 function playRound(playerSelection, computerSelection) {
@@ -90,7 +97,7 @@ function playRound(playerSelection, computerSelection) {
 	} else if (botScore === 0.5) {
 		message = `You tied!`;
 		console.log(`Bot chose ${computerSelection}`);
-	console.log(`You chose! ${playerSelection}`);
+		console.log(`You chose! ${playerSelection}`);
 		console.log(message);
 		return { botScore: 0, playerScore: 0 };
 	} else {
@@ -101,71 +108,68 @@ function playRound(playerSelection, computerSelection) {
 		return { botScore: 0, playerScore: 1 };
 	}
 }
- 
-// this funtion return  a new score based on the winner by incrementing
-function scoreCounter(count){
 
+// this funtion return  a new score based on the winner by incrementing
+function scoreCounter(count) {
 	botWin += count.botScore;
 	playerWin += count.playerScore;
-    console.log( count.botScore);
+	console.log(count.botScore);
 
-	return [botWin,playerWin]
-
+	return [botWin, playerWin];
 }
 
 // this is the  actuall game funtion which all other funtion connects to be run in a loop
-function game(){
-	for(i=0;i<5;i++){
-		
-		score =playRound(userChoice(),computerPlay())
-	    scoreCounter(score)
+function game() {
+	for (i = 0; i < 5; i++) {
+		score = playRound(userChoice(), computerPlay());
+		scoreCounter(score);
 		console.log('scores \n', `Computer:${botWin}`, `${user}:${playerWin}`);
-		console.log(`End of round ${i+1}`)
+		console.log(`End of round ${i + 1}`);
 		if (i < 4) {
- 			alert(`Press OK to play Round ${i + 2}`);
+			alert(`Press OK to play Round ${i + 2}`);
 		}
-
 	}
 }
 
-game()
+game();
 
 // This funtion declares the winener after ever five interation
-function winnerDeclare(){
-	let totalScore = scoreCounter(score)
-			if (totalScore[1] > totalScore[0]) {
-				console.log(
-					`Congrats:) The Overall Winner is ${user} with ${totalScore[1]} points scores`
-				)
-				replay()
-			} else if (totalScore[0] > totalScore[1]) {
-				console.log(
-					`The Overall Winner is Computer with ${totalScore[0]} points scores`
-				)
-				replay()
-			} else {
-				console.log('This was a draw match');
-				replay()
-			}
+function winnerDeclare() {
+	let totalScore = scoreCounter(score);
+	if (totalScore[1] > totalScore[0]) {
+		console.log(
+			`Congrats:) The Overall Winner is ${user} with ${totalScore[1]} points scores`
+		);
+		replay();
+	} else if (totalScore[0] > totalScore[1]) {
+		console.log(
+			`The Overall Winner is Computer with ${totalScore[0]} points scores`
+		);
+		replay();
+	} else {
+		console.log('This was a draw match');
+		replay();
+	}
 }
 
-winnerDeclare()
+winnerDeclare();
 
 // this funtion replays the game  depending on the users reponds after every 5 rounds
-function replay(){
-	let replayConfirm= confirm('Would You like to play again..Press "ok" to replay or "cancle" to quite')
-	if (replayConfirm){
-		while(replayConfirm){
-			botWin=0
-			playerWin=0
-			console.clear()
-			game()
-			replay()
+function replay() {
+	let replayConfirm = confirm(
+		'Would You like to play again..Press "ok" to replay or "cancle" to quite'
+	);
+	if (replayConfirm) {
+		while (replayConfirm) {
+			botWin = 0;
+			playerWin = 0;
+			console.clear();
+			game();
+			replay();
 		}
-	}
-	else{
-		alert("Hope You Enjoyed see You Another time")
-		console.clear()
-		throw console.log("If You decide to play the Game kindly refresh the page")
+	} else {
+		alert('Hope You Enjoyed see You Another time');
+		console.clear();
+		throw console.log('If You decide to play the Game kindly refresh the page');
 	}
 }
